d28e1f43335e54cd25b92a711231dc4a8dc8b8ef
theorudkiewicz <theorudkiewicz@crans.org>
2023-12-24 14:53:39 +0100

    Slight optimization

--- src/compute_u.py
+++ src/compute_u.py
@@ -6,39 +6,39 @@ except ImportError:
     from god_model_tools import count_errors
 
 
-def enumerate_order_info(n_cat: int) -> Iterable[tuple[int, np.ndarray]]:
+def enumerate_order_info(m: int) -> Iterable[tuple[int, np.ndarray]]:
     """
     Enumerate all possible order information for n_cat categories
     Complexity: O(2^n_cat * n_cat)
 
     Args:
-        n_cat: number of categories
+        m: number of categories
 
     Return:
         order_info: all possible order information (n_cat, 2^n_cat)
     """
-    order_info = np.zeros(n_cat, dtype=int)
-    for i in range(2**n_cat):
+    order_info = np.zeros(m, dtype=int)
+    for i in range(2 ** m):
         yield i, order_info
-        for j in range(n_cat):
+        for j in range(m):
             order_info[j] = (order_info[j] + 1) % 2
             if order_info[j] == 1:
                 break
 
 
-def get_all_errors(n_cat: int) -> np.ndarray:
+def get_all_errors(m: int) -> np.ndarray:
     """
     Generate all possible errors for n_cat categories
     Complexity: O(2^n_cat * n_cat)
 
     Args:
-        n_cat: number of categories
+        m: number of categories
 
     Return:
         errors: all possible errors (n_cat, n_cat)
     """
-    all_errors = np.zeros((2**n_cat, n_cat), dtype=int)
-    for i, order_info in enumerate_order_info(n_cat):
+    all_errors = np.zeros((2 ** m, m), dtype=int)
+    for i, order_info in enumerate_order_info(m):
         all_errors[i] = count_errors(order_info)
 
     return all_errors
@@ -63,10 +63,10 @@ def compute_u(m: int) -> np.ndarray:
     is_minimal: np.ndarray = np.min(distance, axis=1)[:, None] == distance
     card_min = np.sum(is_minimal, axis=1)
     u = np.zeros((m, m, m + 1))
-    for mu in range(1, m + 1):
-        for x in range(1, m + 1):
-            for i in range(2**m):
-                if is_minimal[i, x - 1]:
+    for x in range(1, m + 1):
+        for i in range(2 ** m):
+            if is_minimal[i, x - 1]:
+                for mu in range(1, m + 1):
                     d = distance[i, mu - 1]
                     u[mu - 1, x - 1, d] += 1 / card_min[i]
     return u
